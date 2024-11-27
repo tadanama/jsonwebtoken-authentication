94983e412092f12664d6d0a608a1b3807dade77a
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-27 14:50:11 +0800

    Change database name

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
 	port: process.env.DB_PORT,
-	database: "todoapp" || process.env.DB_NAME,
+	database: process.env.DB_NAME,
 	max: 20,
 	idleTimeoutMillis: 30000,
 	connectionTimeoutMillis: 2000,
