3823cd2e5dff304c4bc9997c38641f89e29df5f8
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-27 14:48:00 +0800

    Check if user already exist
    - Send error when email or username already exist

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/login", (req, res) => {
 		return res.status(400).json("Email, username or password is required.");
 });
 
-app.post("/signup", (req, res) => {
+app.post("/signup", async (req, res) => {
 	// Get the user data from request body
 	const {
 		body: { email, username, password },
@@ -33,6 +33,23 @@ app.post("/signup", (req, res) => {
 	// Return error if it is
 	if (!email || !username || !password)
 		return res.status(400).json("Email, username or password is required.");
+
+	try {
+		// Check if any user with the provided email or username from client exist
+		const userWithSameEmailOrUsername = await pool.query(
+			"SELECT * FROM users WHERE email = $1 OR username = $2",
+			[email, username]
+		);
+
+		if (userWithSameEmailOrUsername.rowCount !== 0) {
+			return res
+				.status(409)
+				.json("Email or username already exist in database. Try logging in.");
+		}
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json("Error when querying database level 1");
+	}
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
