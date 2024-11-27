28b08457220dce41a7f160ba57c771a94804c5fc
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-27 14:33:01 +0800

    Put database connection on seperate file

---
+++ db.js
@@ -0,0 +1,20 @@
+import env from "dotenv";
+import pg from "pg";
+const { Pool } = pg;
+
+// Enable the use environment variables
+env.config();
+
+// Configure database connection
+const pool = new Pool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	database: "todoapp" || process.env.DB_NAME,
+	max: 20,
+	idleTimeoutMillis: 30000,
+	connectionTimeoutMillis: 2000,
+});
+
+export default pool;
