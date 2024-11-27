76fc01baabb1015e8fdf7d458d10646f2426aed2
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-27 14:16:23 +0800

    Add email in request body on login & signup route

--- index.js
+++ index.js
@@ -26,24 +26,24 @@ app.use(express.json());
 // Route handler for user login
 app.post("/login", (req, res) => {
 	const {
-		body: { username, password },
+		body: { email, username, password },
 	} = req;
 	console.log(username, password);
 
-	if (!username || !password)
+	if (!email || !username || !password)
 		return res.status(400).json("Username and password cannot be empty.");
 });
 
 app.post("/signup", (req, res) => {
 	// Get the user data from request body
 	const {
-		body: { username, password },
+		body: { email, username, password },
 	} = req;
-	console.log(username, password);
+	console.log(email, username, password);
 
 	// Check if the data is empty
 	// Return error if it is
-	if (!username || !password)
+	if (!email || !username || !password)
 		return res.status(400).json({ error: "Username or password is required" });
 });
 
