192d86c31316f8408df7e67e11d708de596f3dda
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-11-27 14:04:09 +0800

    Configure database connection

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import env from "dotenv";
+import pg from "pg";
+const { Pool } = pg;
 
 // Enable the use environment variables
 env.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Configure database connection
+const pool = new Pool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	database: "todoapp" || process.env.DB_NAME,
+	max: 20,
+	idleTimeoutMillis: 30000,
+	connectionTimeoutMillis: 2000,
+});
 
 app.use(express.json());
 
