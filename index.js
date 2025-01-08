c7773ead5b2be9399685816fce739ab9912f0fee
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2025-01-08 11:44:02 +0800

    Add protected routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import env from "dotenv";
 
 import authRoutes from "./routes/authRoutes.js";
+import protectedRoutes from "./routes/protectedRoutes.js";
 
 // Enable the use environment variables
 env.config();
@@ -12,5 +13,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/api/routes", protectedRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
